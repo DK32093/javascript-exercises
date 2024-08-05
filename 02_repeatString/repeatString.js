211b5f749b4eed0ca23f0c93eeb5daa5f6121d3c
DK32093 <107143112+DK32093@users.noreply.github.com>
2024-08-05 15:14:55 -0400

    write repeatString function and pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
 
+const repeatString = function(text, number) {
+    let textString = "";
+    if (number < 0) {
+        textString += 'ERROR';
+    } else {
+        for (i = 1; i <= number; i++) {
+            textString += text; 
+        }
+    }
+    return textString;
 };
+
+repeatString("hey", 3);
+repeatString('hello', 10);
+repeatString('hi', 1);
+repeatString('bye', 0);
+repeatString('goodbye', -1);
 
 // Do not edit below this line
 module.exports = repeatString;
