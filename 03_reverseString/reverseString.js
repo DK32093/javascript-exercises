ed553223115b2c5d8fba9bc0eb6fd4db567c13b1
DK32093 <107143112+DK32093@users.noreply.github.com>
2024-08-06 17:30:39 -0400

    write reverse string function and pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let rev = text.split("");
+    rev.reverse();
+    return rev.join("");
 };
 
 // Do not edit below this line
