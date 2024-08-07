063a99206e6c3ba367cd37e74f130aecd5ba362f
DK32093 <107143112+DK32093@users.noreply.github.com>
2024-08-07 14:12:07 -0400

    create sumAll function and pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (Number.isInteger(num1) & Number.isInteger(num2) & num1 > 0 & num2 > 0) {
+        if (num1 < num2) {
+            let sum = num1
+            for (i = num1 + 1; i <= num2; i++) {
+               sum = sum + i
+            }
+            return sum;
+        } else {
+            let sum = num2
+            for (i = num2 + 1; i <= num1; i++) {
+               sum = sum + i
+            }
+            return sum;
+        }
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
