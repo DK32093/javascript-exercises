b586ab36359ce2385b269da3677ec0793f4583c7
DK32093 <107143112+DK32093@users.noreply.github.com>
2024-08-07 15:00:26 -0400

    create conversion functions and pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(ftemp) {
+  let c = Math.round(((ftemp - 32) * (5/9)) * 10) / 10;
+  return c;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(ctemp) {
+  let f = Math.round((ctemp * (9/5) + 32) * 10) / 10;
+  return f;
 };
 
 // Do not edit below this line
